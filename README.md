8008c1d81d31caafa68f8c26cad4c357ead47935
Aditya Aluru <aditya.aluru@gmail.com>
2023-08-07 23:01:53 +0530

    Fixed signature

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # my-ondc
+
+
+## Steps to deploy this project on firebase
+
+Deploy the full project
+`firebase deploy`
+
+Deploy only the functions in the project
+`firebase deploy --only functions`
+
+Deploy only a specific function (search in this example)
+`firebase deploy --only functions:search`
--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,8 @@ const logger = require("firebase-functions/logger");
 const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
 
+var blake2b = require('blake2b');
+
 initializeApp();
 
 // Create and deploy your first functions
@@ -36,4 +38,28 @@ exports.onSearch = onRequest(async (request, response) => {
   logger.info("Final result", JSON.stringify(writeResult));
   response.json({message: `Message with ID: ${writeResult.id} added.`});
   
+});
+
+exports.search = onRequest(async (request, response) => {
+  
+  const doc = {body: request.body, headers: request.headers};
+  const result = {};
+  logger.info("Received search request", doc);
+
+  //Creating a payload hash
+  result.reqToHash = request.body.test;
+
+/**
+ * Generate UTF-8 byte array from json payload;
+Generate Blake2b hash from UTF-8 byte array;
+Create base64 encoding of Blake2b hash. This becomes the digest for signing;
+Sign the request, using your private signing key, and add the signature to the request authorization heade
+
+ */
+
+
+response.json(result);
+
+
+
 });
